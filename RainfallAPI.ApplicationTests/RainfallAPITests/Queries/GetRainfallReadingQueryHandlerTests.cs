11b7b66f77ec6609f960d8e4fb04cb7664b73fc1
Jasper Manzano <jasper@nyfulcrum.com>
2024-02-22 13:48:57 +0800

    [API-0007] Application Unit Tests

---
+++ RainfallAPI.ApplicationTests/RainfallAPITests/Queries/GetRainfallReadingQueryHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using RainfallAPI.Application.Queries.GetRainfallReadingQuery;
+
+namespace RainfallAPI.ApplicationTests.RainfallAPITests.Queries
+{
+    public class GetRainfallReadingQueryHandlerTests
+    {
+        private readonly GetRainfallReadingQueryHandler _handler;
+
+        public GetRainfallReadingQueryHandlerTests()
+        {
+            _handler = new GetRainfallReadingQueryHandler();
+        }
+
+        [Fact]
+        public async Task Handler_ValidStationIDAndCount_ShouldHaveNoErrors()
+        {
+            // Arrange
+            var validRainfallReadingRequest = new GetRainfallReadingQuery()
+            {
+                StationId = "3680",
+                Count = 10
+            };
+
+            // Act
+            var response = await _handler.Handle(validRainfallReadingRequest, default);
+
+            // Assert
+            Assert.Empty(response.ErrorResponses);
+        }
+
+        [Fact]
+        public async Task Handler_InvalidCount_ShouldHaveErrors()
+        {
+            // Arrange
+            var validRainfallReadingRequest = new GetRainfallReadingQuery()
+            {
+                StationId = "3680",
+                Count = 1000
+            };
+
+            // Act
+            var response = await _handler.Handle(validRainfallReadingRequest, default);
+
+            // Assert
+            Assert.Single(response.ErrorResponses);
+        }
+
+        [Fact]
+        public async Task Handler_EmptyRequest_ShouldHaveErrors()
+        {
+            // Arrange
+            var validRainfallReadingRequest = new GetRainfallReadingQuery()
+            {
+                StationId = ""
+            };
+
+
+            // Act
+            var response = await _handler.Handle(validRainfallReadingRequest, default);
+
+            // Assert
+            Assert.Single(response.ErrorResponses);
+        }
+    }
+}
